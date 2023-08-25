e966796c3a7d0d01062989e81731e887d74a3986
ajay-vikram <ajayvikramp@gmail.com>
2023-08-25 16:05:01 +0530

    Fixed indentation

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -13,12 +13,13 @@ class PredictPipeline:
         try:
             model_path=os.path.join("artifacts","model.pkl")
             preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
-            print("Before Loading")
+
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
-            print("After Loading")
+
             data_scaled=preprocessor.transform(features)
             preds=model.predict(data_scaled)
+            
             return preds
         
         except Exception as e:
@@ -37,17 +38,11 @@ class CustomData:
         writing_score: int):
 
         self.gender = gender
-
         self.race_ethnicity = race_ethnicity
-
         self.parental_level_of_education = parental_level_of_education
-
         self.lunch = lunch
-
         self.test_preparation_course = test_preparation_course
-
         self.reading_score = reading_score
-
         self.writing_score = writing_score
 
     def get_data_as_data_frame(self):
